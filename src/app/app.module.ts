699cc98932a4320823f4ed0045c2526e4caeff1c
Akarsh Gangwar <akarsh.gangwar@stackpro.in>
2022-09-20 16:17:37 +0530

    sorting and searching feature implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatTableModule } from '@angular/material/table'  
+import { MatInputModule } from '@angular/material/input';
 
 import { AppComponent } from './app.component';
+import { MatTableDataSource } from '@angular/material/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSortModule } from '@angular/material/sort';
+// import {MatTableModule} from '@angular/material'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatTableModule,
+    MatSortModule,
+    MatInputModule,
+
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
